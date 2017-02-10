e556969da56e351fd829257d9ee35dda31217f8c
Follpvosten <wolfi@karpador.xyz>
2017-02-10 20:47:39 +0100

    Config file loading parametrisiert

--- src/schneckenrennen/ConfigManager.java
+++ src/schneckenrennen/ConfigManager.java
@@ -15,17 +15,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * A class responsible for loading the snail names, snail race names and
- * general race names ("the values") from the config file.
+ * A class responsible for loading the snail names, snail race names and general
+ * race names ("the values") from config files.
+ *
  * @author Follpvosten
  */
 public final class ConfigManager {
 
     /**
-     * The default config file name. Cannot be changed at the moment.
+     * The default config file path.
      */
-    public static final String DEFAULT_CONFIG = "config.json";
-    
+    public static final String DEFAULT_CONFIG = "./config.json";
+
     /**
      * The key used for the double value of the WettBuero's factor.
      */
@@ -43,84 +44,127 @@ public final class ConfigManager {
      */
     public static final String RACENAMES_KEY = "racenames";
 
+    // Data loaded from the config file
     private static ArrayList<String> snailNames, snailRaces, raceNames;
     private static double betFactor;
-    
+
     /**
      * Exception to be thrown when there are less than 4 snail names
      */
     private static class NotEnoughNamesException extends Exception {}
 
     /**
-     * Loads the values from the saved config file, if existent.
-     * If the config file is missing, a new one is generated.
+     * Loads values from the default config file.
      */
-    public static void loadConfigFile() {
-	File file = new File("./" + DEFAULT_CONFIG);
-	if (file.exists()) {
-	    try {
-		FileInputStream fis = new FileInputStream(file);
-		byte[] data = new byte[(int) file.length()];
-		fis.read(data);
-		fis.close();
-		loadConfigJSON(new String(data, "UTF-8"));
-	    } catch (FileNotFoundException ex) {
-		Logger.getLogger(ConfigManager.class.getName())
-			.log(Level.SEVERE, "Someone deleted a file. Like, really, really fast.", ex);
-		System.out.println("Someone deleted a file. Like, really, really fast.");
-		applyDefaults();
-	    } catch (IOException ex) {
-		Logger.getLogger(ConfigManager.class.getName())
-			.log(Level.SEVERE, "Could not load file content for some reason.", ex);
-		System.out.println("Could not load file content for some reason.");
-		applyDefaults();
-	    } catch (NotEnoughNamesException ex) {
-		Logger.getLogger(ConfigManager.class.getName())
-			.log(Level.SEVERE, "Not enough snail names in the config file!", ex);
-		System.out.println("Not enough snail names in the config file!");
-		applyDefaults();
-	    }
-	} else {
+    public static void loadDefaultConfigFile() {
+	try {
+	    loadConfigFile(DEFAULT_CONFIG);
+	} catch (FileNotFoundException ex) {
 	    Logger.getLogger(ConfigManager.class.getName())
-			.log(Level.INFO, "No config file found, generating a new one.");
+		    .log(Level.INFO, TranslationManager.getTranslation("Config.error.fileNotFound"), ex);
+	    applyDefaults();
+	} catch (IOException ex) {
+	    Logger.getLogger(ConfigManager.class.getName())
+		    .log(Level.SEVERE, TranslationManager.getTranslation("Config.error.ioError"), ex);
+	    applyDefaults();
+	} catch (JSONException ex) {
+	    Logger.getLogger(ConfigManager.class.getName())
+		    .log(Level.SEVERE, TranslationManager.getTranslation("Config.error.jsonInvalid"), ex);
+	    applyDefaults();
+	} catch (NotEnoughNamesException ex) {
+	    Logger.getLogger(ConfigManager.class.getName())
+		    .log(Level.SEVERE, TranslationManager.getTranslation("Config.error.notEnoughNames"), ex);
 	    applyDefaults();
 	}
     }
 
     /**
-     * Generates the default config values and saves them to the config file.
+     * Loads values from the specified config file.
+     * @param filename The path of the config file to get settings from.
+     * @return An empty String if everything went well, errors otherwise
+     */
+    public static String loadSpecificConfig(String filename) {
+	StringBuilder result = new StringBuilder();
+	try {
+	    loadConfigFile(filename);
+	} catch (FileNotFoundException ex) {
+	    String message = TranslationManager.getTranslation("Config.error.selectedFileNotFound");
+	    Logger.getLogger(ConfigManager.class.getName())
+		    .log(Level.INFO, message, ex);
+	    result.append(message).append('\n');
+	} catch (IOException ex) {
+	    String message = TranslationManager.getTranslation("Config.error.ioError");
+	    Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, message, ex);
+	    result.append(message).append('\n');
+	} catch(JSONException ex) {
+	    String message = TranslationManager.getTranslation("Config.error.jsonInvalid");
+	    Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, message, ex);
+	    result.append(message).append('\n');
+	} catch (NotEnoughNamesException ex) {
+	    String message = TranslationManager.getTranslation("Config.error.notEnoughNames");
+	    Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, message, ex);
+	    result.append(message);
+	}
+	return result.toString();
+    }
+
+    /**
+     * Loads the values from the given config file. Does not make any checks and
+     * throws Exceptions for every error.
+     * @param filename The name of the file to be loaded
+     * @throws java.io.FileNotFoundException When the file does not exist
+     * @throws java.io.IOException When the file can't be read for some reason
+     * @throws schneckenrennen.ConfigManager.NotEnoughNamesException
+     */
+    public static void loadConfigFile(String filename)
+	    throws FileNotFoundException, IOException,
+		   JSONException, NotEnoughNamesException {
+	File file = new File(filename);
+
+	FileInputStream fis = new FileInputStream(file);
+	byte[] data = new byte[(int) file.length()];
+	fis.read(data);
+	fis.close();
+	loadConfigJSON(new String(data, "UTF-8"));
+    }
+
+    /**
+     * Generates the default config values and saves them to the config file if
+     * it doesn't exist.
      */
     private static void applyDefaults() {
 	betFactor = 1.5;
-	snailNames = new ArrayList<>(Arrays.asList(new String[]{
-	    "Winfried", "Bertha", "Hannibal", "Hüftgelenk",
-	    "Goliath", "Moses", "Petrus", "Nikolaus",
-	    "Python", "Baumhardt", "Felix", "Osiris",
-	    "Waldi", "Luna", "Schnurri", "Sushi",
-	    "Deidre", "Wolfi", "Blümchen", "Bo-bobo",
-	    "Günther", "Horst-Dieter", "Kevin", "Moritz",
-	    "Anthony", "Buchwirt", "Linda", "Eckhardt"
-	}));
-	raceNames = new ArrayList<>(Arrays.asList(new String[]{
-	    "Rennen des Jahres", "Irgendein Rennen", "Rennen Nummer Drei",
-	    "Das große Kriechen", "Grand Kriech", "Marathon",
-	    "Entenlauf", "Schneckenrennen", "Wettkriechen",
-	    "Aus die Schneck' - Der letzte Tag", "Wanderschnecken auf Eilkurs",
-	    "Ein puitziges Rennen"
-	}));
-	snailRaces = new ArrayList<>(Arrays.asList(new String[]{
-	    "Audi", "LKW", "Airbus", "Schäferhund",
-	    "Bernhardiner", "Dackel", "Perser", "Siam",
-	    "Chihuahua", "Pago-Pago", "Moai", "Baum",
-	    "Kaninchen", "Hase", "Pui", "Mäuschen",
-	    "Pudel", "Affenbrotbaum", "Goldfisch", "U-Boot",
-	    "Bim", "Tram", "Zug", "Kreuzfahrtschiff",
-	    "UFO", "Banane", "Meerschweinchen", "Treppenstufe",
-	    "Schienenersatzverkehr", "Wanderer", "Schwimmer", "Knirps",
-	    "Tänzerin", "Aromalady"
-	}));
+	snailNames = new ArrayList<>(Arrays.asList(
+		"Winfried", "Bertha", "Hannibal", "Hüftgelenk",
+		"Goliath", "Moses", "Petrus", "Nikolaus",
+		"Python", "Baumhardt", "Felix", "Osiris",
+		"Waldi", "Luna", "Schnurri", "Sushi",
+		"Deidre", "Wolfi", "Blümchen", "Bo-bobo",
+		"Günther", "Horst-Dieter", "Kevin", "Moritz",
+		"Anthony", "Buchwirt", "Linda", "Eckhardt"
+	));
+	raceNames = new ArrayList<>(Arrays.asList(
+		"Rennen des Jahres", "Irgendein Rennen", "Rennen Nummer Drei",
+		"Das große Kriechen", "Grand Kriech", "Marathon",
+		"Entenlauf", "Schneckenrennen", "Wettkriechen",
+		"Aus die Schneck' - Der letzte Tag", "Wanderschnecken auf Eilkurs",
+		"Ein puitziges Rennen"
+	));
+	snailRaces = new ArrayList<>(Arrays.asList(
+		"Audi", "LKW", "Airbus", "Schäferhund",
+		"Bernhardiner", "Dackel", "Perser", "Siam",
+		"Chihuahua", "Pago-Pago", "Moai", "Baum",
+		"Kaninchen", "Hase", "Pui", "Mäuschen",
+		"Pudel", "Affenbrotbaum", "Goldfisch", "U-Boot",
+		"Bim", "Tram", "Zug", "Kreuzfahrtschiff",
+		"UFO", "Banane", "Meerschweinchen", "Treppenstufe",
+		"Schienenersatzverkehr", "Wanderer", "Schwimmer", "Knirps",
+		"Tänzerin", "Aromalady"
+	));
 
-	saveConfig();
+	if (!new File(DEFAULT_CONFIG).exists()) {
+	    saveConfig();
+	}
     }
 
     /**
@@ -130,7 +174,7 @@ public final class ConfigManager {
 	try {
 	    JSONObject configJSON = new JSONObject();
 	    configJSON.put(BETFACTOR_KEY, betFactor);
-	    
+
 	    {
 		JSONArray currentArray = new JSONArray();
 		for (String s : snailNames) {
@@ -150,7 +194,7 @@ public final class ConfigManager {
 		}
 		configJSON.put(RACENAMES_KEY, currentArray);
 	    }
-	    
+
 	    File file = new File(DEFAULT_CONFIG);
 	    FileWriter fw = new FileWriter(file, false);
 	    fw.write(configJSON.toString(2));
@@ -169,40 +213,36 @@ public final class ConfigManager {
 
     /**
      * Loads the values from a config JSON string.
+     *
      * @param jsonString The JSON string from the config file
      */
-    private static void loadConfigJSON(String jsonString) throws NotEnoughNamesException {
-	try {
-	    JSONObject json = new JSONObject(jsonString);
-	    betFactor = json.optDouble(BETFACTOR_KEY, 1.5);
-	    {
-		snailNames = new ArrayList<>();
-		JSONArray namesJsonArray = json.getJSONArray(SNAILNAMES_KEY);
-		if(namesJsonArray.length() < 4) {
-		    throw new NotEnoughNamesException();
-		}
-		for (int i = 0; i < namesJsonArray.length(); i++) {
-		    snailNames.add(namesJsonArray.getString(i));
-		}
+    private static void loadConfigJSON(String jsonString)
+	    throws JSONException, NotEnoughNamesException {
+	JSONObject json = new JSONObject(jsonString);
+	betFactor = json.optDouble(BETFACTOR_KEY, 1.5);
+	{
+	    snailNames = new ArrayList<>();
+	    JSONArray namesJsonArray = json.getJSONArray(SNAILNAMES_KEY);
+	    if (namesJsonArray.length() < 4) {
+		throw new NotEnoughNamesException();
 	    }
-	    {
-		snailRaces = new ArrayList<>();
-		JSONArray racesJsonArray = json.getJSONArray(SNAILRACES_KEY);
-		for (int i = 0; i < racesJsonArray.length(); i++) {
-		    snailRaces.add(racesJsonArray.getString(i));
-		}
+	    for (int i = 0; i < namesJsonArray.length(); i++) {
+		snailNames.add(namesJsonArray.getString(i));
 	    }
-	    {
-		raceNames = new ArrayList<>();
-		JSONArray racesJsonArray = json.getJSONArray(RACENAMES_KEY);
-		for (int i = 0; i < racesJsonArray.length(); i++) {
-		    raceNames.add(racesJsonArray.getString(i));
-		}
+	}
+	{
+	    snailRaces = new ArrayList<>();
+	    JSONArray racesJsonArray = json.getJSONArray(SNAILRACES_KEY);
+	    for (int i = 0; i < racesJsonArray.length(); i++) {
+		snailRaces.add(racesJsonArray.getString(i));
 	    }
-	} catch (JSONException ex) {
-	    Logger.getLogger(ConfigManager.class.getName())
-		    .log(Level.SEVERE, "Somehow failed to read JSON object.", ex);
-	    System.out.println("Somehow failed to read JSON object.");
+	}
+	{
+	    raceNames = new ArrayList<>();
+	    JSONArray racesJsonArray = json.getJSONArray(RACENAMES_KEY);
+	    for (int i = 0; i < racesJsonArray.length(); i++) {
+		raceNames.add(racesJsonArray.getString(i));
+	    }
 	}
     }
 
